545f10c2842295d15730fa3a865d543daeece048
feldenserra <154560189+feldenserra@users.noreply.github.com>
2026-01-25 15:26:13 -0800

    feat: simplify next.config.ts by removing experimental server components configuration

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  serverExternalPackages: ["@prisma/client"],
   reactCompiler: true,
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
 };
 
 export default nextConfig;
